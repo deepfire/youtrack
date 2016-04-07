a4cd966bbce6a45cd922febb72bfcaf0f38ffb78
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2016-04-08 02:03:10 +0300

    version:  v0.0.3

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "youtrack";
-  version = "0.0.2";
+  version = "0.0.3";
   src = ./.;
   libraryHaskellDepends = [
     aeson base base-unicode-symbols bytestring HsOpenSSL http-client

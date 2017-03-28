73116b4f44d6f6d220af5d2ffea67cca010f83b2
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-03-28 19:46:32 +0300

    nix:  add optparse-applicative

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
-{ mkDerivation, aeson, base, base-unicode-symbols, bytestring
-, HsOpenSSL, http-client, http-client-openssl, lens, mtl, parsers, QuickCheck
-, safe, scientific, split, stdenv, text, time, trifecta, unordered-containers
-, utf8-string, vector, wreq
+{ stdenv, mkDerivation
+, aeson, base, base-unicode-symbols, bytestring
+, HsOpenSSL, http-client, http-client-openssl, lens, mtl, optparse-applicative
+, parsers, QuickCheck, safe, scientific, split, text, time, trifecta
+, unordered-containers, utf8-string, vector, wreq
 }:
 mkDerivation {
   pname = "youtrack";
-  version = "0.0.6";
+  version = "0.0.7";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base base-unicode-symbols bytestring HsOpenSSL http-client
-    http-client-openssl lens mtl parsers QuickCheck safe scientific text split time trifecta
+    aeson base base-unicode-symbols bytestring
+    HsOpenSSL http-client http-client-openssl lens mtl optparse-applicative
+    parsers QuickCheck safe scientific split text split time trifecta
     unordered-containers utf8-string vector wreq
   ];
   homepage = "https://github.com/deepfire/youtrack";
